6fcdb1000905c314c23ffc1761ca8416ef10dfd6
pablowinter <pablo.winter@gmail.com>
2024-03-30 22:11:14 -0300

    Programando clases y validacines

---
+++ src/app/student-form/student-form.component.ts
@@ -0,0 +1,59 @@
+import { JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-student-form',
+  templateUrl: './student-form.component.html',
+  styleUrl: './student-form.component.scss'
+})
+export class StudentFormComponent {
+    studentForm:FormGroup;
+
+    constructor(private formBuilder:FormBuilder){
+      this.studentForm=this.formBuilder.group({
+        name:this.formBuilder.control(''),
+        lastName:this.formBuilder.control(''),
+        birthDate:this.formBuilder.control(''),
+        email:this.formBuilder.control('',
+        [Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}'),
+        Validators.required]),
+        adreess:this.formBuilder.control(''),
+        password:this.formBuilder.control(''),
+        repeatPassword:this.formBuilder.control('')
+      });
+    }
+
+    onSubmit():void{
+      alert(JSON.stringify(this.studentForm.value))
+    }
+
+    get nameControl() {
+      return this.studentForm.get('name');
+    }
+
+    get lastNameControl() {
+      return this.studentForm.get('lastName');
+    }
+
+    get birthDateControl(){
+      return this.studentForm.get('birthDate');
+    }
+
+    get emailControl(){
+      return this.studentForm.get('email');
+    }
+
+    get adreessControl(){
+      return this.studentForm.get('adreess');
+    }
+
+    get passwordControl(){
+      return this.studentForm.get('password');
+    }
+
+    get repeatPasswordControl(){
+      return this.studentForm.get('repeatPassword');
+    }
+
+}
